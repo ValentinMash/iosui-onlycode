b59a7456d5dcf73424a8ccfc0aa70dbfb05b009f
ValentinMash <vs.mashutin@gmail.com>
2022-05-03 22:05:31 +0300

    Проведена подготовка проектя для выполнения ДЗ

---
+++ Navigation/Navigation/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Valentin Mashutin on 03.05.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        return true
+    }
+
+}
+
