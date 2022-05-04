4a127d1ad6c214647c1786cb7b19e6e524cde6f6
ValentinMash <vs.mashutin@gmail.com>
2022-05-04 21:42:16 +0300

    Добавлена кнопка на FeedVC

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -11,10 +11,19 @@ class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .yellow
+        makeButton()
     }
     
-
-
+    func makeButton(){
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.center = view.center
+        button.setTitle("Переход", for: .normal)
+        button.backgroundColor = .brown
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc func tapAction() {
+        
+    }
 }
