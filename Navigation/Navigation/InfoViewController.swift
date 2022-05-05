c19e612dd5cdbc00234456f142090878db26f55b
ValentinMash <vs.mashutin@gmail.com>
2022-05-05 22:09:22 +0300

    Добавлен Alert

--- Navigation/Navigation/InfoViewController.swift
+++ Navigation/Navigation/InfoViewController.swift
@@ -11,19 +11,24 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addButton()
+      
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addButton(){
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.center = view.center
+        button.setTitle("Назад", for: .normal)
+        button.backgroundColor = .orange
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
     }
-    */
-
+    @objc func tapAction (){
+        let alert = UIAlertController(title: "Назад", message: "Точно назад?", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Выйти", style: .default)
+        let cancel = UIAlertAction(title: "Отмена", style: .destructive)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+   
 }
