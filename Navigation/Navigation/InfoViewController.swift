e22b61f6504f56441e807b6231bffa214d188bf0
ValentinMash <vs.mashutin@gmail.com>
2022-05-06 21:54:34 +0300

    Покрасил InfoVC в зеленый цвет

--- Navigation/Navigation/InfoViewController.swift
+++ Navigation/Navigation/InfoViewController.swift
@@ -11,6 +11,7 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
         addButton()
       
     }
